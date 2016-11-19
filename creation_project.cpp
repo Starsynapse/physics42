f06c35988005375a08ecff248278a6652b24a6be
starsynapse <starsynapse@gmail.com>
2016-11-19 06:33:08 +0000

    creating the array for number of charrges

--- creation_project.cpp
+++ creation_project.cpp
@@ -1,7 +1,27 @@
 /*
  *  Author: Eduardo Zamora
- *  Program: creation project
+ *  Program: creation_project.cpp
  *  Date created: 11 November 2016
  *  Last edited: 11 November 2016
  */
 
+#include "creation_project_declarations.h"
+#include "creation_project_definitions.cpp"
+
+int main()
+{
+    int numberOfCharges;
+    
+    cout << "Size of array?";
+    cin >> numberOfCharges;
+    
+    string *a = new string[numberOfCharges];
+    
+    fillArrayString(a, numberOfCharges);
+    
+    
+    
+    delete [] a;
+    
+    return 0;
+}
